c67a2930c1c68bf9864b2c181b329a63258b44f8
ganadosx <przemek4121@gmail.com>
2022-01-06 19:49:12 +0100

    1.5.3 - Fix -> UserRepository

--- src/main/java/com/ganados/fitness/application/FitnessApplication/repositories/user/UserRepository.java
+++ src/main/java/com/ganados/fitness/application/FitnessApplication/repositories/user/UserRepository.java
@@ -2,7 +2,9 @@ package com.ganados.fitness.application.FitnessApplication.repositories.user;
 
 import com.ganados.fitness.application.FitnessApplication.model.user.User;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends CrudRepository<User, Long> {
 
     User findByEmail(final String email);
